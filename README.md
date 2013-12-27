a0f57be4a1dccacbce7c56b5dcdf1b9e42034178
Yusuke Wada <yusuke@kamawada.com>
2013-12-28 07:59:25 +0900

    make templates!

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Melodie - It's new $module
+
+# SYNOPSIS
+
+    use Melodie;
+
+# DESCRIPTION
+
+Melodie is ...
+
+# LICENSE
+
+Copyright (C) Yusuke Wada.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Yusuke Wada <yusuke@kamawada.com>
---
+++ lib/Melodie.pm
@@ -0,0 +1,39 @@
+package Melodie;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Melodie - It's new $module
+
+=head1 SYNOPSIS
+
+    use Melodie;
+
+=head1 DESCRIPTION
+
+Melodie is ...
+
+=head1 LICENSE
+
+Copyright (C) Yusuke Wada.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke@kamawada.comE<gt>
+
+=cut
+
